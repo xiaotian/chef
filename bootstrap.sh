7ec9656134993e99e4be2afe5bee44354a4f3f8a
Xiaotian Guo <guoxiaotian@gmail.com>
2012-05-24 19:00:39 -0700

    use packaged ruby to speed things up

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,15 +7,16 @@ apt-get -y update
 apt-get -y install git-core build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev
 
 if ! command_exists ruby ; then
-  echo 'Installing ruby from source ...'
-  ruby_src=ruby-1.9.3-p194
-  cd /tmp
-  wget -O ${ruby_src}.tar.gz ftp://ftp.ruby-lang.org/pub/ruby/1.9/${ruby_src}.tar.gz
-  tar -xvzf ${ruby_src}.tar.gz
-  cd ${ruby_src}/
-  ./configure --prefix=/usr/local
-  make
-  make install
+  # echo 'Installing ruby from source ...'
+  # ruby_src=ruby-1.9.3-p194
+  # cd /tmp
+  # wget -O ${ruby_src}.tar.gz ftp://ftp.ruby-lang.org/pub/ruby/1.9/${ruby_src}.tar.gz
+  # tar -xvzf ${ruby_src}.tar.gz
+  # cd ${ruby_src}/
+  # ./configure --prefix=/usr/local
+  # make
+  # make install
+  apt-get -y install ruby
 else
   echo 'found ruby.'
 fi
