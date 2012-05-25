8c94ea14f1ec3b76e5c97147fda5b42d7689e23f
Xiaotian Guo <guoxiaotian@gmail.com>
2012-05-24 19:17:45 -0700

    added back build-essential

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,22 +4,7 @@ command_exists () {
 }
 
 apt-get -y update
-# apt-get -y install git-core build-essential zlib1g-dev libssl-dev libreadline-dev libyaml-dev
-apt-get -y install git-core ruby
-
-# if ! command_exists ruby ; then
-#   echo 'Installing ruby from source ...'
-#   ruby_src=ruby-1.9.3-p194
-#   cd /tmp
-#   wget -O ${ruby_src}.tar.gz ftp://ftp.ruby-lang.org/pub/ruby/1.9/${ruby_src}.tar.gz
-#   tar -xvzf ${ruby_src}.tar.gz
-#   cd ${ruby_src}/
-#   ./configure --prefix=/usr/local
-#   make
-#   make install
-# else
-#   echo 'found ruby.'
-# fi
+apt-get -y install build-essential git-core ruby
 
 if ! command_exists chef-solo ; then
   echo 'Installing chef ...'
