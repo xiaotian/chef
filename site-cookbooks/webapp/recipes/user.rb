a4ac03b0755846675d65540176460f1ee596b158
Xiaotian Guo <guoxiaotian@gmail.com>
2012-05-26 11:08:19 -0700

    add deployer's public key

--- site-cookbooks/webapp/recipes/user.rb
+++ site-cookbooks/webapp/recipes/user.rb
@@ -9,6 +9,24 @@ user deployer_name do
   action :create
 end
 
+# create .ssh directory for deployer
+directory File.join('/home', deployer_name, '.ssh') do # default is /u/apps
+  owner deployer_name
+  group deployer_name
+  mode "0700"
+  action :create
+  recursive true
+end
+
+# create authorized_keys for deployer
+cookbook_file File.join('/home', deployer_name, '.ssh', 'authorized_keys') do
+  source 'id_rsa_deploy.pub'
+  mode "0700"
+  owner deployer_name
+  group deployer_name
+  action :create_if_missing
+end
+
 #create user for webapp
 username = node.webapp.user.name || node.webapp.name
 
